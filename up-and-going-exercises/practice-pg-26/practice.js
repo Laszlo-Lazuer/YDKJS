988ea68ef5b34317b4601558fa1ee5dc2431724a
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-09 01:06:08 -0600

    Practice 1

---
+++ up-and-going-exercises/practice-pg-26/practice.js
@@ -0,0 +1,35 @@
+const TAX = .08;
+const CHECKING_BALANCE = prompt('Account balance?');
+const PHONE_PRICE = 99.99;
+const ACCESORY_PRICE = 9.99;
+const SPENDING_THRESHOLD = prompt('Spending threshold? (Acct balance: ' + CHECKING_BALANCE + ')'); //CHECKING_BALANCE * .8;
+
+function phonePurchase() {
+  let cost = 0;
+  let numPhones = 0;
+  let numAcc = 0;
+  let costA = 0;
+  let affordable = '';
+
+  while (cost < SPENDING_THRESHOLD) {
+    // console.log('cost: ' + cost + ' Threshold: ' + SPENDING_THRESHOLD);
+    ++numPhones;
+    cost += numPhones + PHONE_PRICE;
+    costA = cost + ACCESORY_PRICE;
+
+    if (costA < SPENDING_THRESHOLD) {
+      numAcc += 1;
+      cost += ACCESORY_PRICE;
+    } else {
+      break;
+    }
+  }
+
+  cost = cost + (cost * TAX);
+
+  affordable = (cost <= CHECKING_BALANCE) ? 'YES':'NO';
+
+  return 'purchased: ' + numPhones + ' acc: ' + numAcc + ' Total Cost: $' + cost.toFixed(2) + ' Affordable?: ' + affordable;
+}
+
+document.write('Checking balance: $' + CHECKING_BALANCE + ' Threshold: $' + SPENDING_THRESHOLD + '<br>' + phonePurchase());
